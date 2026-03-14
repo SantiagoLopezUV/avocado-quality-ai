3c520b34db9dc7db23faf601a7af414e31975508
SantiagoLopezUV <santiago.lopez.ramirez@correounivalle.edu.co>
2026-03-14 18:26:26 -0500

    remove test endpoint

--- app/api/v1/routes/analysis.py
+++ app/api/v1/routes/analysis.py
@@ -1,8 +1,47 @@
 from fastapi import APIRouter, File, UploadFile
+from app.services import predictor_service
+# Importamos la función de análisis de aguacate desde el archivo analysis_service.py
+from app.services.analysis_service import AnalysisService
+import shutil
+import os
 
 router = APIRouter()
 
+################################################################################
+
+
+#   Este endpoint es de prueba para verificar que el servicio de análisis de imágenes funciona correctamente.
+# @router.post("/analyze")
+# async def analyze_image(file: UploadFile = File(...)):
+#     # Placeholder for image analysis logic
+#     return {"message": "Endpoint de analisis - modelo de IA no implementado"}
+
+################################################################################
+
+#   Se comenta porque no se tiene el modelo de IA entrenado aun, 
+# pero se deja la estructura para cuando se implemente el modelo.
+
+analysis_service = AnalysisService()
+
 @router.post("/analyze")
+#definimos la funcion asiconcrona para analizar la imagen del aguacate
 async def analyze_image(file: UploadFile = File(...)):
-    # Placeholder for image analysis logic
-    return {"message": "Endpoint de analisis - modelo de IA no implementado"}
+    #SE COMENTA PORQUE NO SE TIENE EL MODELO DE IA ENTRENADO AUN
+
+    #Se guarda la imagen recibida temporalmente en la carpeta data/raw, para luego ser procesada por el predictor
+    os.makedirs("data/raw", exist_ok=True)  # Crear la carpeta si no existe
+    temp_path = f"data/raw/{file.filename}"
+    with open(temp_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    report = await analysis_service.analyze_avocado(temp_path)
+
+    # Eliminar la imagen temporal después de procesarla
+    if os.path.exists(temp_path):
+        os.remove(temp_path)
+
+    return{
+        "filename": file.filename,
+        "analysis_report": report,
+        "message": "Analisis completado"
+    }
--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 from app.api.v1 import api_router
 from app.core.config import settings
-#Prueba de conexión a la base de datos
-from sqlalchemy import text
-from app.core.database import engine
 
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -12,46 +9,36 @@ app = FastAPI(
     version="0.1.0"
     
 )
-# # Configuración de CORS para permitir solicitudes desde frontend
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=settings.ALLOW_ORIGINS,  # Se puede configurar en .env
-#     #allow_origins=["*"],  # El asterisco deja que cualquier React se conecte
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+# Configuración de CORS para permitir solicitudes desde frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOW_ORIGINS,  # Se puede configurar en .env
+    #allow_origins=["*"],  # El asterisco deja que cualquier React se conecte
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# # Configuración de CORS para permitir solicitudes desde frontend
-# # app.add_middleware(
-# #     CORSMiddleware,
-# #     allow_origins=["*"], #se cambia por URL del frontend 
-# #     allow_credentials=True,
-# #     allow_methods=["*"],
-# #     allow_headers=["*"],
-# # )
+# Configuración de CORS para permitir solicitudes desde frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], #se cambia por URL del frontend 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
 
 
 
-# #-----------------------------------------------------#
+#-----------------------------------------------------#
 
-# @app.get("/")
-# async def root():
-#     return {
-#         "message": "Bienvenido a Avocado Quality AI ",
-#         "docs": "/docs",
-#         "status": "Running"
-#     }
+@app.get("/")
+async def root():
+    return {
+        "message": "Bienvenido a Avocado Quality AI ",
+        "docs": "/docs",
+        "status": "Running"
+    }
 
-
-
-@app.get("/test-db")
-def test_db():
-    try:
-        with engine.connect() as conn:
-            conn.execute(text("SELECT 1"))
-        return {"message": "Conexión a la base de datos exitosa"}
-    except Exception as e:
-        return {"message": "Error al conectar a la base de datos", "error": str(e)}
