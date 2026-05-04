340317c802c09746262a0920d7cf9c8f0852d27f
ospina <alejandro.ospina.lopez@correounivalle.edu.co>
2026-05-03 22:33:33 -0500

    fix(backend): update CORS allowed origins to support frontend dev ports

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,11 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     UPLOAD_DIR: str = "uploads"
-    allow_origins: list[str] = ["http://localhost:5173"]  # Permitir todas las URLs por defecto, se puede configurar en .env
+    allow_origins: list[str] = ["http://localhost:5173",
+                                "http://localhost:5174",
+                                "http://127.0.0.1:5173",
+                                "http://127.0.0.1:5174"
+                                ]  # Permitir todas las URLs por defecto, se puede configurar en .env
 
     class Config:
         env_file = ".env"
